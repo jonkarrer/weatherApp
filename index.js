fcaecefb7586d9fd4f0c1d1cdcd58bf6e004e709
jonkarrer <66031598+jonkarrer@users.noreply.github.com>
2021-01-07 09:51:11 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let endpoint = 'https://api.openweathermap.org/data/2.5/weather?q=';
+/* eslint-disable indent */
+const endpoint = 'https://api.openweathermap.org/data/2.5/weather?q=';
 
-const weatherArr = [];
 const temprature = document.getElementById('number');
 const situation = document.getElementById('situation');
 const time = document.getElementById('time');
@@ -8,91 +8,91 @@ const searchBar = document.getElementById('location');
 const localTimeZone = new Date();
 const moon = document.querySelector('.moon');
 const sun = document.querySelector('.sun');
-searchBar.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-     event.preventDefault();
-     updateCityLocation();
-    };
-});
+
 function updateCityLocation() {
     const apiCity = searchBar.value;
     const apiKey = '&APPID=f0fda7206d1601a5d39960d1c3c15600';
-    let newpoint = `${endpoint}${apiCity}${apiKey}`;
+    const newpoint = `${endpoint}${apiCity}${apiKey}`;
     fetch(newpoint)
-        .then(response => response.json())
-        .then(data => {
-            let description = data.weather[0].description;
-            situation.innerText = 
-            description.charAt(0).toUpperCase() +
-            description.slice(1, description.length);
+        .then((response) => response.json())
+        .then((data) => {
+            const { description } = data.weather[0];
+            situation.innerText = description.charAt(0).toUpperCase()
+            + description.slice(1, description.length);
 
-            let temp = data.main.temp;
-            let degreeF = (temp - 273.15) * 9/5 + 32;
+            const { temp } = data.main;
+            const degreeF = ((temp - 273.15) * 9) / 5 + 32;
             temprature.innerText = Math.floor(degreeF);
 
-            let currentHours = localTimeZone.getHours();
+            const currentHours = localTimeZone.getHours();
             let currentMin = localTimeZone.getMinutes();
-            if(currentMin < 10){
-                currentMin = '0' + currentMin;
-            };
-            let localZone = localTimeZone.getTimezoneOffset()/-60;
-            let targetZone = data.timezone/3600;
-            let timeDiff = targetZone - localZone;
+            if (currentMin < 10) {
+                currentMin = `0${currentMin}`;
+            }
+            const localZone = localTimeZone.getTimezoneOffset() / -60;
+            const targetZone = data.timezone / 3600;
+            const timeDiff = targetZone - localZone;
             let newTime = currentHours + timeDiff;
             let displayedTime;
-            if(newTime > 12 && newTime < 24) {
+            if (newTime > 12 && newTime < 24) {
                 newTime -= 12;
-                displayedTime = newTime.toString() + ':' + currentMin.toString();
-                time.innerText = displayedTime + 'pm';
-                if(newTime > 6){
+                displayedTime = `${newTime.toString()}:${currentMin.toString()}`;
+                time.innerText = `${displayedTime}pm`;
+                if (newTime > 6) {
                     sun.style.display = 'none';
                     moon.style.display = 'block';
-                }else{
+                } else {
                     sun.style.display = 'block';
                     moon.style.display = 'none';
-                };
-            }else if (newTime > 24){
+                }
+            } else if (newTime > 24) {
                 newTime -= 24;
-                if(newTime > 6 && newTime < 17){
+                if (newTime > 6 && newTime < 17) {
                     sun.style.display = 'block';
                     moon.style.display = 'none';
-                };
-                if(newTime < 6){
+                }
+                if (newTime < 6) {
                     sun.style.display = 'none';
                     moon.style.display = 'block';
-                };
-                if(newTime > 12 && newTime < 24) {
+                }
+                if (newTime > 12 && newTime < 24) {
                     newTime -= 12;
-                    displayedTime = newTime.toString() + ':' + currentMin.toString();
-                    time.innerText = displayedTime + 'pm';
-                    if(newTime > 6){
+                    displayedTime = `${newTime.toString()}:${currentMin.toString()}`;
+                    time.innerText = `${displayedTime}pm`;
+                    if (newTime > 6) {
                         sun.style.display = 'block';
                         moon.style.display = 'none';
-                    }else{
+                    } else {
                         sun.style.display = 'block';
                         moon.style.display = 'none';
-                    };
-                }else{
-                    displayedTime = newTime.toString() + ':' + currentMin.toString();
-                    time.innerText = displayedTime + 'am';
-                    if(newTime < 7){
+                    }
+                } else {
+                    displayedTime = `${newTime.toString()}:${currentMin.toString()}`;
+                    time.innerText = `${displayedTime}am`;
+                    if (newTime < 7) {
                         sun.style.display = 'none';
                         moon.style.display = 'block';
-                    }else{
+                    } else {
                         sun.style.display = 'block';
-                        moon.style.display = 'none'; 
-                    };
-                };
-            }else{
-                displayedTime = newTime.toString() + ':' + currentMin.toString();
-                time.innerText = displayedTime + 'am';
-                if(newTime < 7){
+                        moon.style.display = 'none';
+                    }
+                }
+            } else {
+                displayedTime = `${newTime.toString()}:${currentMin.toString()}`;
+                time.innerText = `${displayedTime}am`;
+                if (newTime < 7) {
                     sun.style.display = 'none';
                     moon.style.display = 'block';
-                }else{
+                } else {
                     sun.style.display = 'block';
-                    moon.style.display = 'none'; 
-                };
-            };   
+                    moon.style.display = 'none';
+                }
+            }
         });
-};
+}
+searchBar.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+     event.preventDefault();
+     updateCityLocation();
+    }
+});
